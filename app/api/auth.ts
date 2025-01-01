6ad150502e029b983d81f4fb6f1e9c5aa513516e
mofagaa <94887720+mofagaa@users.noreply.github.com>
2025-01-01 19:13:34 +0800

    Update auth.ts

--- app/api/auth.ts
+++ app/api/auth.ts
@@ -42,7 +42,7 @@ export function auth(req: NextRequest, modelProvider: ModelProvider) {
   if (serverConfig.needCode && !serverConfig.codes.has(hashedCode) && !apiKey) {
     return {
       error: true,
-      msg: !accessCode ? "empty access code" : "wrong access code",
+      msg: !accessCode ? "未输入访问密码！" : "错误的访问密码！",
     };
   }
 
